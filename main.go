70db163e95f686951dea04aae52410feacb92ef9
Konstantin <konstantin@goncharuk.info>
2026-03-24 12:21:10 +0100

    release CI

--- main.go
+++ main.go
@@ -9,7 +9,18 @@ import (
 	"github.com/kooler/MiddayCommander/internal/app"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Printf("mdc %s (%s) built %s\n", version, commit, date)
+		os.Exit(0)
+	}
+
 	p := tea.NewProgram(
 		app.New(),
 		tea.WithAltScreen(),
